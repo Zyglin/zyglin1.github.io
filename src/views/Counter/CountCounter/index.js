bcb806456ee06e54729e61365e0963c37d703ba6
Zyglin <zyglin@mail.ru>
2019-09-06 18:20:07 +0300

    Finish Third Task

--- src/views/Counter/CountCounter/index.js
+++ src/views/Counter/CountCounter/index.js
@@ -1,25 +1,25 @@
 /* eslint-disable react/jsx-indent */
-import React from 'react'
-import PropTypes from 'prop-types'
-import { styled } from '@material-ui/styles'
-import Button from '@material-ui/core/Button'
-import styles from './styles'
-import CounterContainer from '../../../containers/CounterContainer'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { styled } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+import styles from './styles';
+import CounterContainer from '../../../containers/CounterContainer';
 
-const StyledButton = styled(Button)(styles.button)
+const StyledButton = styled(Button)(styles.button);
 
-const StyledButtonForReset = styled(Button)(styles.buttonReset)
+const StyledButtonForReset = styled(Button)(styles.buttonReset);
 
 function PushComponent(buttonBinding, countCounter) {
-  const rows = []
+  const rows = [];
   for (let i = 0; i < countCounter; i += 1) {
-    rows.push(<CounterContainer buttonBinding={buttonBinding} countState={countCounter} key={i} />)
+    rows.push(<CounterContainer buttonBinding={buttonBinding} countState={countCounter} key={i} />);
   }
-  return rows
+  return rows;
 }
 
 const countCounter = props => {
-  const rows = PushComponent(props.buttonBinding, props.countCounter)
+  const rows = PushComponent(props.buttonBinding, props.countCounter);
   return (
     <div>
       <h1>{props.countCounter}</h1>
@@ -30,8 +30,8 @@ const countCounter = props => {
       </StyledButtonForReset>
       {rows}
     </div>
-  )
-}
+  );
+};
 
 countCounter.propTypes = {
   handleDecrementCounter: PropTypes.func.isRequired,
@@ -39,6 +39,6 @@ countCounter.propTypes = {
   handleResetCounter: PropTypes.func.isRequired,
   countCounter: PropTypes.number.isRequired,
   buttonBinding: PropTypes.number.isRequired,
-}
+};
 
-export default countCounter
+export default countCounter;
